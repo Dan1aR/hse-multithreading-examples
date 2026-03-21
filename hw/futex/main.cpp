285cee5e529dcbb2df628d9aa0d9101a12414a1d
Dan1ar-aiphoria <d.andreev@aiforiatech.com>
2026-03-21 14:13:42 +0000

    remove main

--- hw/futex/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "condition_variable.h"
-
-#include <iostream>
-
-int main() {
-    [[maybe_unused]] hw::futex::FutexConditionVariable conditionVariable;
-    std::cout << "hw/futex scaffold is ready" << std::endl;
-    return 0;
-}
